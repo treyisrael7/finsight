7101381c711cff7ebb54c72fa8a118ec303854ae
treyisraeliii <wisrael@clemson.edu>
2025-03-26 13:16:55 -0400

    Initial commit for Finlingo setup

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,12 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
 };
 
 export default config;
